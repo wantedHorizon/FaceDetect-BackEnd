a465d34ed9f9bb82ff002dde40bca3553a06f807
WantedHorizon <eliran.amzalag@gmail.com>
2020-01-15 13:50:19 +0200

    fix connection string

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const img = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true
        
     }
